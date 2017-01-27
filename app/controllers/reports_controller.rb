e20a16e8bebfae3685e052d98ace6234204797e0
Ellen Marie Dash <me@duckie.co>
2017-01-26 20:24:29 -0500

    [WIP]

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,5 +8,7 @@ class ReportsController < ApplicationController
     @repo = params[:repo_id]
     @date = params[:date]
     @report = Report.repo(@repo).on_date(@date).first
+    @html = HowIs.generate_report(repository: @repo,
+                                  from: ???)
   end
 end
