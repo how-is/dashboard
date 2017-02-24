c75fc094752464df07d87207374371f7692cbb99
Andre Arko <andre@arko.net>
2017-02-24 11:22:48 -0800

    default to rubygems/bundler without REPOSITORIES

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,5 +1,5 @@
 class ReposController < ApplicationController
   def index
-    @repos = ENV['REPOSITORIES'].split(',').sort
+    @repos = ENV.fetch('REPOSITORIES', 'rubygems/rubygems,bundler/bundler').split(',').sort
   end
 end
